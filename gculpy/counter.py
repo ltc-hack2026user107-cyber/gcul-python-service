c8da7ef628e446d40ae6174480f947f9bcd0de17
konankip123 <126575261+konankip123@users.noreply.github.com>
2026-07-27 11:08:26 +0100

    Add files via upload

---
+++ gculpy/counter.py
@@ -0,0 +1,24 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import gcul
+
+class Counter(gcul.Contract):
+    """Example contract implementing a counter."""
+
+    value: int
+
+    def increment(self) -> None:
+        """Increments the counter value by 1."""
+        self.value += 1
